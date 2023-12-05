36371322e2023aa9dbb589dd9499c65e3970c5b3
laithsleit <laithsleit@gmail.com>
2023-12-06 01:17:36 +0300

    1:17-6/12/2023

--- New folder (3)/dashboard/js/users.js
+++ New folder (3)/dashboard/js/users.js
@@ -74,3 +74,56 @@ switchMode.addEventListener('change', function () {
 		document.body.classList.remove('dark');
 	}
 })
+// Function to fetch data from the API
+function fetchData() {
+    fetch('http://localhost/api-AMM/api/user/select.php')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Handle the parsed JSON data
+            console.log('Data:', data);
+            updateUsersTable(data);
+        })
+        .catch(error => {
+            // Check if response is defined before accessing its properties
+            const responseStatus = error.response ? error.response.status : 'N/A';
+            console.error(`Error fetching users: ${error.message}`, 'Response status:', responseStatus);
+
+            // If there's no response object, handle the error accordingly
+            if (!error.response) {
+                console.error('Network error or invalid JSON response.');
+            }
+        });
+}
+
+// Function to update the users table
+function updateUsersTable(users) {
+    const tableBody = document.querySelector('.table-data tbody');
+
+    // Clear existing table rows
+    tableBody.innerHTML = '';
+
+    // Loop through users and add rows to the table
+    users.forEach(user => {
+        const newRow = document.createElement('tr');
+        newRow.innerHTML = `
+            <td>
+                <p>${user.Username}</p>
+            </td>
+            <td>${user.Email}</td>
+            <td><span class=" pending status"><a><i class='bx bxs-trash' ></i></a></span></td>
+        `;
+
+        // Append the new row to the table body
+        tableBody.appendChild(newRow);
+    });
+}
+
+// Fetch data when the page loads
+document.addEventListener('DOMContentLoaded', () => {
+    fetchData();
+});
